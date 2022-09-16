82bff0c213ecd167eb512ea84a97331e8921dbbf
Mark Wheldon <wheldon@un.org>
2022-09-16 11:30:31 -0400

    `get_country_summary_results()` now `get_indicator_summary_results()` and can load aggregates, etc.

--- R/results.R
+++ R/results.R
@@ -22,7 +22,7 @@
 ##'
 ##' @export
 get_model_param_quantiles <- function(run_name = NULL, output_dir = NULL, root_dir = NULL,
-                         verbose = FALSE) {
+                                      verbose = FALSE) {
 
     output_dir <-
         output_dir_wrapper(run_name = run_name, output_dir = output_dir,
@@ -40,8 +40,8 @@ get_model_param_quantiles <- function(run_name = NULL, output_dir = NULL, root_d
 ##' @author Mark Wheldon
 ##' @export
 get_countries_model_params_q <- function(...) {
-    warning("'get_countries_model_params_q' is DEPRACATED and will soon be removed! Please use 'get_model_quantiles' instead.")
-    get_model_quantiles(...)
+    warning("'get_countries_model_params_q' is DEPRACATED and will soon be removed! Please use 'get_model_param_quantiles' instead.")
+    get_model_param_quantiles(...)
 }
 
 
@@ -54,25 +54,57 @@ get_countries_model_params_q <- function(...) {
 ##'
 ##' @inheritParams get_output_dir
 ##' @inheritParams get_csv_res
+##' @param aggregate Return country or aggregate results. Only the
+##'     \dQuote{UNPDaggregate}s are available in this format.
+##' @param stat Which statistics should be loaded? Allowable values
+##'     are \code{"std"} for \dQuote{standard} statistics, and
+##'     \code{"age_ratio"} to return the age ratios.
+##' @param adjusted Loads original results (\dQuote{orig}), adjusted
+##'     medians only (\dQuote{adj}).
 ##' @return A list with posterior quantiles of indicators, by country.
 ##' @author Mark Wheldon
 ##'
 ##' @family Get results from rda files
 ##'
 ##' @export
-get_country_summary_results <- function(run_name = NULL, output_dir = NULL, root_dir = NULL,
-                         verbose = FALSE) {
+get_indicator_summary_results <- function(run_name = NULL, output_dir = NULL, root_dir = NULL,
+                                          aggregate = c("country", "aggregate"),
+                                          stat = c("std", "age_ratio"),
+                                          adjusted = c("orig", "adj"),
+                                          verbose = FALSE) {
+
+    ## Checks
 
     output_dir <-
         output_dir_wrapper(run_name = run_name, output_dir = output_dir,
                            root_dir = root_dir, verbose = verbose,
                            post_processed = TRUE, countrytrajectories = FALSE,
                            made_results = FALSE)
+    aggregate <- match.arg(aggregate)
+    stat <- match.arg(stat)
+    adjusted <- match.arg(adjusted)
+
+    if (identical(stat, "age_ratio") && identical(adjusted, "adj"))
+        stop("'stat' is 'age_ratio' and 'adjusted' = 'adj'. There are no adjusted results for age ratios.")
+
+    ## Get Files
 
     if (is_all_women_run(output_dir = output_dir))
-        fname <- "res.country.all.women.rda"
-    else fname <- "res.country.rda"
-    full_fpath <- file.path(output_dir, fname)
+        fname <- paste0("res.", aggregate, ".all.women")
+    else fname <- paste0("res.", aggregate)
+
+    if (identical(adjusted, "adj")) {
+        if (identical(aggregate, "aggregate")) {
+            fname <- gsub("aggregate", "UNPDaggregate", fname, fixed = TRUE)
+        }
+        fname <- paste0(fname, ".adj-mod_tot_unmet")
+    } else {
+        if (identical(stat, "age_ratio")) {
+            fname <- paste0(fname, ".age.ratio")
+        }
+    }
+
+    full_fpath <- file.path(output_dir, paste0(fname, ".rda"))
 
     tmp_env <- new.env()
     if (verbose) on.exit(message("Loaded '", full_fpath, "'."), add = TRUE, after = FALSE)
